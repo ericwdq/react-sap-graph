d706f0c76b93f2d8320c08f1c220826452c4e532
i319701 <eric.wang04@sap.com>
2019-12-18 17:05:44 +0800

    Add sap logo and test button

--- src/features/home/WelcomePage.js
+++ src/features/home/WelcomePage.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Alert, Icon, Button } from 'fundamental-react';
 
-export default function WelcomePage() {
+export default function WelcomePage(props) {
   return (
     <div className="home-welcome-page">
       <header className="app-header">
-        <img src={require('../../images/rekit-react.png')} className="rekit-logo" alt="logo" />
-        <h1 className="app-title">Welcome to Rekit React</h1>
+        <img src={require('../../images/sap-logo.svg')} className="sap-logo" alt="logo" />
+        <h1 className="app-title">
+          Welcome to React and SAP Graph {'  '}
+          <a href="https://www.graph.sap" rel="nofollow" target="_blank">
+            graph.sap
+          </a>
+        </h1>
       </header>
       <div className="app-intro">
         <h3>To get started:</h3>
@@ -22,6 +28,22 @@ export default function WelcomePage() {
             <Link to="/examples">/examples</Link>
           </li>
         </ul>
+        <br />
+        <Alert style={{ margin: '10px', display: 'none' }} dismissible type="error">
+          <Icon glyph="message-error" />
+          Testing fundamental styles &nbsp;&nbsp;
+          <a className="fd-link" href="/#learn">
+            Learn More
+          </a>
+        </Alert>
+        <Button
+          option="emphasized"
+          onClick={() => {
+            props.history.push(`/examples`);
+          }}
+        >
+          Go to /examples
+        </Button>
       </div>
     </div>
   );
